3ca81b08994d1d9bc21a9389ce136a2a0882ed4a
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-10-13 10:46:02 -0300

    Add files via upload

--- public/cadastro.js
+++ public/cadastro.js
@@ -1,17 +1,29 @@
 function verSenhaAluno() {
-    const inputao = document.getElementById('prsenha');
+    const inputao = document.getElementById('alsenha');
+    const divzao = document.getElementById('aldivsenha');
+    const imagizona = document.getElementById('alzoi');
     if (inputao.type === 'password') {
         inputao.type = 'text';
+        divzao.style.backgroundColor = '#ffda33';
+        imagizona.src = 'imagens/vesim.png';
     } else {
         inputao.type = 'password';
+        divzao.style.backgroundColor = '#e4e4e4';
+        imagizona.src = 'imagens/venao.png';
     }
 }
 
 function verSenhaProf() {
     const inputao = document.getElementById('prsenha');
+    const divzao = document.getElementById('prdivsenha');
+    const imagizona = document.getElementById('przoi');
     if (inputao.type === 'password') {
         inputao.type = 'text';
+        divzao.style.backgroundColor = '#ffda33';
+        imagizona.src = 'imagens/vesim.png';
     } else {
         inputao.type = 'password';
+        divzao.style.backgroundColor = '#e4e4e4';
+        imagizona.src = 'imagens/venao.png';
     }
 }

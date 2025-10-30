40fd16b1047ec65b21b99d1b0c88bf213221c673
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-10-30 10:38:14 -0300

    Add files via upload

--- public/selecconta.js
+++ public/selecconta.js
@@ -1,5 +1,5 @@
 // Função para listar todos os alunos, oq era sobre buscar foi comentado
-async function listarContas() {
+async function listarAluno() {
     /*
     const email = document.getElementById('email').value.trim();  // Pega o valor do email digitado no input
 
@@ -9,56 +9,60 @@ async function listarContas() {
     }
     */
 
-    let url = '/contas';  // URL padrão para todos os clientes
-
-    try {
-        const response = await fetch(url);
-        const aluno = await response.json();
-
-        const tabela = document.getElementById('tabelaaluno');
-        tabela.innerHTML = ''; // Limpa a tabela antes de preencher
-
-        if (aluno.length === 0) {
-            // Caso não encontre cadastros, exibe uma mensagem
-            tabela.innerHTML = '<tr><td colspan="2">Nenhum cadastro encontrado.</td></tr>';
-        } else {
-            aluno.forEach(aluno => {
-                const linha = document.createElement('tr');
-                linha.innerHTML = `
-                    <td>${aluno.al_id}</td>
-                    <td>${aluno.al_email}</td>
-                    <td>${aluno.al_nome}</td>
-                    <td>${aluno.al_senha}</td>
-                `;
-                tabela.appendChild(linha);
-            });
-        }
-    } catch (error) {
-        console.error('Erro ao listar cadastros:', error);
-    }
-}
     let url = '/aluno';  // URL padrão para todos os clientes
 
     try {
         const response = await fetch(url);
         const aluno = await response.json();
 
-        const tabela = document.getElementById('tabelaaluno');
+        const tabela = document.getElementById('secalunos');
         tabela.innerHTML = ''; // Limpa a tabela antes de preencher
 
         if (aluno.length === 0) {
             // Caso não encontre cadastros, exibe uma mensagem
-            tabela.innerHTML = '<tr><td colspan="2">Nenhum cadastro encontrado.</td></tr>';
+            tabela.innerHTML = 'Nenhum aluno encontrado.';
         } else {
             aluno.forEach(aluno => {
-                const linha = document.createElement('tr');
-                linha.innerHTML = `
-                    <td>${aluno.al_id}</td>
-                    <td>${aluno.al_email}</td>
-                    <td>${aluno.al_nome}</td>
-                    <td>${aluno.al_senha}</td>
+                tabela.innerHTML = `
+                    <div>${aluno.al_nome}</div>
                 `;
-                tabela.appendChild(linha);
+                tabela.appendChild(tabela);
+            });
+        }
+    } catch (error) {
+        console.error('Erro ao listar cadastros:', error);
+    }
+}
+
+// Função para listar todos os alunos, oq era sobre buscar foi comentado
+async function listarProf() {
+    /*
+    const email = document.getElementById('email').value.trim();  // Pega o valor do email digitado no input
+
+    if (email) {
+        // Se CPF foi digitado, adiciona o parâmetro de consulta
+        url += `?email=${email}`;
+    }
+    */
+
+    let url = '/prof';  // URL padrão para todos os clientes
+
+    try {
+        const response = await fetch(url);
+        const prof = await response.json();
+
+        const tabela = document.getElementById('secprof');
+        tabela.innerHTML = ''; // Limpa a tabela antes de preencher
+
+        if (prof.length === 0) {
+            // Caso não encontre cadastros, exibe uma mensagem
+            tabela.innerHTML = 'Nenhum profesor encontrado.';
+        } else {
+            prof.forEach(prof => {
+                tabela.innerHTML = `
+                    <div>${prof.pr_nome}</div>
+                `;
+                tabela.appendChild(tabela);
             });
         }
     } catch (error) {

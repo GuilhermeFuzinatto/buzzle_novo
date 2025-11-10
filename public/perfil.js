9c355d333185482049f44e639be385d34d122b92
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-11-10 09:25:40 -0300

    Add files via upload

--- public/perfil.js
+++ public/perfil.js
@@ -2,5 +2,5 @@ const user = JSON.parse(localStorage.getItem('usuario'));
 if (user) {
     const spacenome = document.getElementById('teunome');
     
-    spacenome.textContent = '${user.nome}';
+    spacenome.textContent = user.nome;
 } 

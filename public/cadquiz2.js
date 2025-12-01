f4e12b8222f8541763ca7608f2b056bedc285b52
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-12-01 06:08:14 -0300

    Add files via upload

--- public/cadquiz2.js
+++ public/cadquiz2.js
@@ -1,2 +1,27 @@
 const user = JSON.parse(localStorage.getItem('usuario'));
 
+let numeroPergunta = 1;
+
+/*
+function criarPergunta() {
+    const sec = document.getElementById("secmain");
+
+    const div = document.createElement("div");
+    div.className = "pergunta";
+
+    div.innerHTML = `
+        <h3>Pergunta ${numeroPergunta}</h3>
+        <input type="text" class="input-enunciado" placeholder="Digite o enunciado">
+
+        <div class="alternativas">
+            <input type="text" class="alt" placeholder="Alternativa 1">
+            <input type="text" class="alt" placeholder="Alternativa 2">
+            <input type="text" class="alt" placeholder="Alternativa 3">
+            <input type="text" class="alt" placeholder="Alternativa 4">
+        </div>
+    `;
+
+    sec.appendChild(div);
+    numeroPergunta++;
+}
+*/

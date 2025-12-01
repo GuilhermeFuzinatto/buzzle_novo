3815ee52ea9cf60138a85a050bfd5ad4b1ff885a
Guilherme Fuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-12-01 09:32:32 -0300

    Delete turma.js

--- turma.js
+++
@@ -1,8 +0,0 @@
-const turminha = JSON.parse(localStorage.getItem('turma'));
-if (turminha) {
-    const spacenome = document.getElementById('turmanome');
-    const spacedesc = document.getElementById('turmadesc');
-    
-    spacenome.textContent = turminha.nome;
-    spacedesc.textContent = turminha.desc;
-} 
